f81e735b7088268d1aadf545088d164756dcdf65
obaed <obaedul@redlimesolutions.com>
2024-01-11 12:18:24 +0600

    Players expense calculation & player selected list  added

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ const footballPlayers = [
     },
 ]
 
-
+//Show Player data Start
 const playerList = document.getElementById("player-list");
 
 footballPlayers.forEach(player => {
@@ -73,10 +73,56 @@ footballPlayers.forEach(player => {
                             Assist</p>
                         <div class="select-btn text-center mt-3">
                             <button 
-                                class="btn btn-success w-100 rounded-0 ">SELECT</button>
+                                class="btn btn-success w-100 rounded-0 "  onclick="addPlayer(this)">SELECT</button>
                         </div>
                     </div>
                 </div>
     `;
      playerList.appendChild(singlePlayerDiv);
 })
+
+//Show Player data end
+
+
+/* === Common Function Code Start ==== */
+// All Input Field Value
+function inputExpenseAmount(inputId){
+    const inputBudgetField = document.getElementById(inputId);
+    const inputBudgetText = inputBudgetField.value;
+    const inputBudget = parseFloat(inputBudgetText);
+    return inputBudget;        
+}
+// Player List Length
+function playerListLength(elementId){
+    const playerList = document.getElementById(elementId);
+    const playersOnlist = playerList.getElementsByTagName('li');
+    const players = playersOnlist.length;
+    return players;
+}
+/* === Common Function Code End ==== */
+
+
+/* === Player Selected on field === */
+//Add player on Selected field
+function addPlayer(element){
+    const playerList = playerListLength("add-player")
+    const addPlayerList = document.querySelector("#add-player");
+    if(playerList < 5){
+        const li = document.createElement("li");
+        const playerName = element.parentNode.parentNode.children[0].innerText;
+        li.innerText = playerName;
+        addPlayerList.appendChild(li);
+        element.disabled = "true";
+    }
+}
+
+/** === Player Expense Calculation  === **/
+function playersExpense(){
+    const singlePlayerExpense = inputExpenseAmount("per-player-expense");
+    const playerList = playerListLength("add-player")
+    const playerExpenseText =  document.querySelector("#players-expense");
+
+    const playersExpense = playerList * singlePlayerExpense;
+    playerExpenseText.innerText = playersExpense;
+    return playersExpense;
+}
